ee4c8f5270d593c1f7caf6d9241931a381d8281e
Thomas Minier <tminier01@gmail.com>
2015-10-31 09:37:10 +0100

    fix little error in CustomSearch & adjust travis.yml

--- galactichorse/src/main/webapp/js/services/CustomSearch.js
+++ galactichorse/src/main/webapp/js/services/CustomSearch.js
@@ -5,26 +5,31 @@
  * A service who handle the interactions with the Custom Search API
  */
 angular.module('GalacticHorseSearch.services')
-  .service('CustomSearch',["$http", function($http){
+  .service('CustomSearch',["$http", "$q", function($http, $q){
     var srv = this;
-  	var url = "https://ajax.googleapis.com/ajax/services/search";
   	var apiKey = "AIzaSyA6FZQslvsM5r1Sm9aWTiczBChi1y8_zfU";
-    var indexPage = 1;
-    var search = "";
 
     /*
      * Method who request the datas from Custom search API
      */
-  	var fetch = function() {
-      return $http.get("https://www.googleapis.com/customsearch/v1?key=" +apiKey+ "&cx=017576662512468239146:omuauf_lfve&q=" + search +"&start=" +indexPage);
+  	var fetch = function(indexPage) {
+      var deferred = $q.defer();
+      $http.get("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=017576662512468239146:omuauf_lfve&q=" + srv.search +"&start=" + indexPage)
+        .then(function(datas) {
+          srv.details = datas;
+          deferred.resolve(datas);
+        }, function(error) {
+          deferred.reject(error);
+        });
+      return deferred.promise;
     }
 
     /*
      * Method who init the search & fetch the first page of the search
      */
     srv.init = function(searchString){
-      search = searchString;
-  		return fetch();
+      srv.search = searchString;
+  		return fetch(1);
   	}
 
     /*
@@ -32,7 +37,7 @@ angular.module('GalacticHorseSearch.services')
      */
     srv.nextPage = function(){
         indexPage = srv.details.queries.nextPage[0].startIndex;
-        return fetch();
+        return fetch(indexPage);
     }
 
     /*
@@ -40,7 +45,7 @@ angular.module('GalacticHorseSearch.services')
      */
     srv.previousPage = function(){
         indexPage = srv.details.queries.previousPage[0].startIndex;
-        return fetch();
+        return fetch(indexPage);
     }
 
 }]);
