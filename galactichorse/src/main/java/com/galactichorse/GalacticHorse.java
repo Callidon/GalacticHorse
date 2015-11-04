85e4d5a056b88e1de842aa57912288f2398b9028
Giraudux <giraudux@gmail.com>
2015-11-04 19:48:03 +0100

    add endpoint

---
+++ galactichorse/src/main/java/com/galactichorse/GalacticHorse.java
@@ -0,0 +1,34 @@
+package com.galactichorse;
+
+import com.google.api.server.spi.config.*;
+import com.google.appengine.api.datastore.*;
+
+@Api(name = "galactichorse",
+        version = "v1",
+        namespace = @ApiNamespace(ownerDomain = "galactichorse.com",
+                ownerName = "galactichorse.com",
+                packagePath=""))
+public class GalacticHorse {
+    @ApiMethod(name = "post", httpMethod = ApiMethod.HttpMethod.POST)
+    public void post(@Named("url") String url, @Named("data") String data) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity entity = new Entity("Entity", url);
+        entity.setProperty("url", url);
+        entity.setProperty("data", data);
+        datastore.put(entity);
+    }
+    @ApiMethod(name = "get", httpMethod = ApiMethod.HttpMethod.GET)
+    public UrlDataBean get(@Named("url") String url) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity entity;
+        UrlDataBean ret = new UrlDataBean();
+        try {
+            entity = datastore.get(KeyFactory.createKey("Entity", url));
+            ret.setUrl((String) entity.getProperty("url"));
+            ret.setData((String) entity.getProperty("data"));
+        } catch (EntityNotFoundException e) {
+            ;
+        }
+        return ret;
+    }
+}
