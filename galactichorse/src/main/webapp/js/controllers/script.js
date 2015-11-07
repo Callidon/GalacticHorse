b7d11a75c071d90cc05d4e902a53e8a4684c9fe4
P1erreGaultier <pit.gaultier@free.fr>
2015-11-07 17:28:23 +0100

    change on js stuff

--- galactichorse/src/main/webapp/js/controllers/script.js
+++ galactichorse/src/main/webapp/js/controllers/script.js
@@ -1,15 +1,26 @@
-angular.module('myApp', ['GalacticHorseSearch.services'])
+/**
+ * @ngdoc module
+ * @name GalacticHorseChrome.module:GalacticHorse
+ * @description
+ * A controller for a first display of searches
+ */
+angular.module('GalacticHorse', ['GalacticHorseSearch.services'])
   .controller('SearchController',["$http", 'CustomSearch', function($http,CustomSearch){
     var pendingTask;
     var ctrl = this;
     var indexPage =1;
 
-
+    /*
+     * We set the search as "lectures" by default
+     */
     if(ctrl.search === undefined){
       ctrl.search = "lectures";
       update();
     }
-
+    
+     /*
+     * fonction which update the page on a change in the search input
+     */
     ctrl.change = function(){
       if(pendingTask){
         clearTimeout(pendingTask);
@@ -17,24 +28,27 @@ angular.module('myApp', ['GalacticHorseSearch.services'])
       pendingTask = setTimeout(update, 800);
     };
 
+     /*
+     * fonction which get the search from the input by an http get, and put it in ctrl.details
+     */
     function update(){
      CustomSearch.init(ctrl.search).then(function(data){ctrl.details = data},function(error){console.log(error)});
     }
 
-    ctrl.update = function(search){
-      ctrl.search = search.Title;
-      ctrl.change();
-    };
 
-
+     /*
+     * fonction which display the next page of the actual search
+     */
     ctrl.nextPage = function(){
       CustomSearch.nextPage().then(function(data){ctrl.details = data},function(error){console.log(error)});
     }  
-    
+
+     /*
+     * fonction which display the previous page of the actual search
+     */    
     ctrl.previousPage = function(){
       CustomSearch.previousPage().then(function(data){ctrl.details = data},function(error){console.log(error)});
-    }  
-
+    }
 
   }]);
   
