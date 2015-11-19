e02c1aeb8408f767919667702cac210b5a916f8d
Giraudux <giraudux@gmail.com>
2015-11-19 21:56:41 +0100

    add json-ld servlet

--- galactichorse/src/main/java/com/galactichorse/Ontology.java
+++ galactichorse/src/main/java/com/galactichorse/Ontology.java
@@ -1,67 +1,20 @@
 package com.galactichorse;
 
-import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
 import com.hp.hpl.jena.rdf.model.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
+import javax.servlet.http.*;
+import java.io.*;
 
-@Api(name = "ontology")
-public class Ontology {
-    @ApiMethod(name = "get", httpMethod = ApiMethod.HttpMethod.GET)
-    public Collection<String> get() throws Exception {
-        File f = new File("WEB-INF/accessibility-ontology.ttl");
+public class Ontology extends HttpServlet {
+    private static final String ONTOLOGY_PATH = "WEB-INF/accessibility-ontology.ttl";
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //@TODO: check file extension
+        File f = new File(ONTOLOGY_PATH);
         InputStream is = new FileInputStream(f);
         Model model = ModelFactory.createOntologyModel();
-        Collection<String> c = new ArrayList<String>();
         model.read(is, null, "TURTLE");
-
-        // list the statements in the Model
-        StmtIterator iter = model.listStatements();
-
-        // print out the predicate, subject and object of each statement
-        while (iter.hasNext()) {
-            Statement stmt = iter.nextStatement();  // get next statement
-            Resource subject = stmt.getSubject();     // get the subject
-            Property predicate = stmt.getPredicate();   // get the predicate
-            RDFNode object = stmt.getObject();      // get the object
-
-            c.add(subject.toString());
-            c.add(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                c.add(object.toString());
-            } else {
-                // object is a literal
-                c.add(" \"" + object.toString() + "\"");
-            }
-        }
-
-        return c;
-    }
-}
-
-class ElementBean {
-    private Collection<ElementBean> elements;
-    private Map<String, Object> subCategories;
-
-    public Collection<ElementBean> getElements() {
-        return elements;
-    }
-
-    public void setElements(Collection<ElementBean> elements) {
-        this.elements = elements;
-    }
-
-    public Map<String, Object> getSubCategories() {
-        return subCategories;
-    }
-
-    public void setSubCategories(Map<String, Object> subCategories) {
-        this.subCategories = subCategories;
+        model.write(resp.getOutputStream(), "JSON-LD");
     }
 }
