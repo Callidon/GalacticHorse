bdf8526c5e4f21648b7fe902228a0504285a6d63
Giraudux <giraudux@gmail.com>
2015-11-24 23:28:39 +0100

    add method to check/put jsonld

--- galactichorse/src/main/java/com/galactichorse/Search.java
+++ galactichorse/src/main/java/com/galactichorse/Search.java
@@ -2,7 +2,16 @@ package com.galactichorse;
 
 import com.google.api.server.spi.config.*;
 import com.google.appengine.api.datastore.*;
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.ReasonerRegistry;
+import com.hp.hpl.jena.reasoner.ValidityReport;
+import org.apache.jena.riot.Lang;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,15 +20,37 @@ import java.util.Map;
 @Api(name = "search")
 public class Search {
     private static final String ENTITY_KIND_URL_TAGS = "url_tags";
+    private static final String ENTITY_KIND_URL_JSONLD = "url_jsonld";
 
     @ApiMethod(name = "put", httpMethod = ApiMethod.HttpMethod.POST)
     public void put(UrlTagsBean urlTags) throws Exception {
         URL url = new URL(urlTags.getUrl());
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Entity entity = new Entity(ENTITY_KIND_URL_TAGS, urlTags.getUrl());
-        for (Map.Entry<String, Object> tag: urlTags.getTags().entrySet()) {
+        for (Map.Entry<String, Object> tag : urlTags.getTags().entrySet()) {
             entity.setProperty(tag.getKey(), tag.getValue());
         }
+        datastore.put(entity);
+    }
+
+    @ApiMethod(name = "put_jsonld", httpMethod = ApiMethod.HttpMethod.POST)
+    public void putJsonld(UrlJsonldBean urlJsonld) throws Exception {
+        URL url = new URL(urlJsonld.getUrl());
+        Model ontology = Ontology.getOntologyModel();
+        Model model = ModelFactory.createDefaultModel();
+        model.read(new ByteArrayInputStream(urlJsonld.getJsonld().getBytes()), null, Lang.JSONLD.getLabel());
+        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+        reasoner = reasoner.bindSchema(ontology);
+        InfModel infmodel = ModelFactory.createInfModel(reasoner, model);
+        ValidityReport validityReport = infmodel.validate();
+        if (!validityReport.isValid()) {
+            throw new Exception();
+        }
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity entity = new Entity(ENTITY_KIND_URL_JSONLD, urlJsonld.getUrl());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        infmodel.write(baos, Lang.JSONLD.getLabel());
+        entity.setProperty("jsonld", baos.toString());
         datastore.put(entity);
     }
 
@@ -31,7 +62,7 @@ public class Search {
         for (String url : urls.getUrls()) {
             keys.add(KeyFactory.createKey(ENTITY_KIND_URL_TAGS, url));
         }
-        for (Map.Entry<Key, Entity> entry: datastore.get(keys).entrySet()) {
+        for (Map.Entry<Key, Entity> entry : datastore.get(keys).entrySet()) {
             UrlTagsBean urlTags = new UrlTagsBean();
             urlTags.setUrl(entry.getKey().getName());
             urlTags.setTags(entry.getValue().getProperties());
@@ -71,5 +102,26 @@ class UrlsBean {
 
     public void setUrls(Collection<String> urls) {
         this.urls = urls;
+    }
+}
+
+class UrlJsonldBean {
+    private String url;
+    private String jsonld;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getJsonld() {
+        return jsonld;
+    }
+
+    public void setJsonld(String jsonld) {
+        this.jsonld = jsonld;
     }
 }
