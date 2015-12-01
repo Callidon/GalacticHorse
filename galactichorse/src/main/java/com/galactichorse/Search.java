57a775fd24c9c20256a5c55381e1ddce008373d4
Giraudux <giraudux@gmail.com>
2015-12-01 18:51:55 +0100

    add link relation

--- galactichorse/src/main/java/com/galactichorse/Search.java
+++ galactichorse/src/main/java/com/galactichorse/Search.java
@@ -1,5 +1,6 @@
 package com.galactichorse;
 
+import com.galactichorse.beans.LinkBean;
 import com.galactichorse.beans.RequestBean;
 import com.galactichorse.beans.ResponseBean;
 import com.google.api.server.spi.config.*;
@@ -32,7 +33,6 @@ public class Search {
     private static final String ENTITY_PROPERTY_MODEL = "model";
 
     /**
-     *
      * @return
      * @throws IOException
      */
@@ -42,12 +42,16 @@ public class Search {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         getOntologyModel().write(baos, MODEL_LANGUAGE.getLabel());
         response.setOntology(baos.toString());
-        //response.set_links();
+
+        Map<String, LinkBean> links = new HashMap<String, LinkBean>();
+        links.put("self", getOntologyLink());
+        links.put("nexts", getPutLink());
+        response.set_links(links);
+
         return response;
     }
 
     /**
-     *
      * @param urlModel
      * @return
      * @throws Exception
@@ -56,6 +60,7 @@ public class Search {
     public ResponseBean putUrlModel(RequestBean urlModel) throws Exception {
         ResponseBean response = new ResponseBean();
         URL url = new URL(urlModel.getUrl());
+
         Model ontology = getOntologyModel();
         Model model = ModelFactory.createDefaultModel();
         model.read(new ByteArrayInputStream(urlModel.getModel().getBytes()), null, MODEL_LANGUAGE.getLabel());
@@ -66,40 +71,50 @@ public class Search {
         if (!validityReport.isValid()) {
             throw new Exception("invalid model");
         }*/
+
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Entity entity = new Entity(ENTITY_KIND_URL_MODEL, url.toString());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        /*inf*/model.write(baos, MODEL_LANGUAGE.getLabel());
+        /*inf*/
+        model.write(baos, MODEL_LANGUAGE.getLabel());
         entity.setProperty(ENTITY_PROPERTY_MODEL, new Text(baos.toString()));
         datastore.put(entity);
-        //response.set_links();
+
+        Map<String, LinkBean> links = new HashMap<String, LinkBean>();
+        links.put("self", getPutLink());
+        links.put("next", getSearchLink());
+        response.set_links(links);
+
         return response;
     }
 
     /**
-     *
      * @param urls
      * @return
      */
     @ApiMethod(name = "get", httpMethod = ApiMethod.HttpMethod.POST)
     public ResponseBean searchUrls(RequestBean urls) {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         ResponseBean response = new ResponseBean();
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Collection<Key> keys = new ArrayList<Key>();
-        Map<String,String> urlModels = new HashMap<String,String>();
+        Map<String, String> urlModels = new HashMap<String, String>();
         for (String url : urls.getUrls()) {
             keys.add(KeyFactory.createKey(ENTITY_KIND_URL_MODEL, url));
         }
         for (Map.Entry<Key, Entity> entry : datastore.get(keys).entrySet()) {
-            urlModels.put(entry.getKey().getName(), ((Text)(entry.getValue().getProperty(ENTITY_PROPERTY_MODEL))).getValue());
+            urlModels.put(entry.getKey().getName(), ((Text) (entry.getValue().getProperty(ENTITY_PROPERTY_MODEL))).getValue());
         }
         response.setUrlModels(urlModels);
-        //response.set_links();
+
+        Map<String, LinkBean> links = new HashMap<String, LinkBean>();
+        links.put("self", getSearchLink());
+        links.put("next", getOntologyLink());
+        response.set_links(links);
+
         return response;
     }
 
     /**
-     *
      * @return
      * @throws FileNotFoundException
      */
@@ -109,5 +124,32 @@ public class Search {
         OntModel model = ModelFactory.createOntologyModel();
         model.read(inputStream, null, ONTOLOGY_LANGUAGE.getLabel());
         return model;
+    }
+
+    /**
+     * @return
+     */
+    public static LinkBean getOntologyLink() {
+        LinkBean link = new LinkBean();
+        link.setHref("/ontology");
+        return link;
+    }
+
+    /**
+     * @return
+     */
+    public static LinkBean getPutLink() {
+        LinkBean link = new LinkBean();
+        link.setHref("/put");
+        return link;
+    }
+
+    /**
+     * @return
+     */
+    public static LinkBean getSearchLink() {
+        LinkBean link = new LinkBean();
+        link.setHref("/get");
+        return link;
     }
 }
