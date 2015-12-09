72ffd96c7b9f728601a6b094bf1e4bd09d49f2d6
Thomas Minier <tminier01@gmail.com>
2015-12-09 14:50:38 +0100

    translate ontology in extension

--- extension/js/app.js
+++ extension/js/app.js
@@ -1,11 +1,55 @@
 
 angular.module("GalacticHorseChrome",
 	[
+
 		"GalacticHorseChrome.controllers",
 		"GalacticHorseChrome.directives",
-		"GalacticHorseChrome.services"
+		"GalacticHorseChrome.services",
+		"pascalprecht.translate"
 	])
-	.run(["GoogleAuth", "$http", function(GoogleAuth, $http) {
+	.config(["$translateProvider", function($translateProvider) {
+
+		$translateProvider.translations("en", {
+			"access:ParkingType" : "Parking type",
+			"access:BlueBadge" : "Blue badge",
+			"access:PayAndDisplay" : "Classic parking",
+			"access:Guide" : "Available guides",
+			"access:AudioGuide" : "Audio guide",
+			"access:VisioGuide" : "Visual guide",
+			"access:EntranceOpeningType" : "Entrance opening type",
+			"access:ManualDoor" : "Manual door",
+			"access:PoweredDoor" : "Powered door",
+			"access:AutomaticDoor" : "Automatic door",
+			"access:FloorAccessType" : "Floor access type",
+			"access:Elevator" : "Elevator",
+			"access:Stairs" : "Stairs",
+			"access:EaseofAccess" : "Ease of access",
+			"access:EasyAccess" : "Easy access",
+			"access:DifficultAccess" : "Difficult access"
+		});
+
+		$translateProvider.translations("fr", {
+			"access:ParkingType" : "Type de parking",
+			"access:BlueBadge" : "Carte bleue",
+			"access:PayAndDisplay" : "Classique",
+			"access:Guide" : "Guide(s) disponible(s)",
+			"access:AudioGuide" : "Audio-guide",
+			"access:VisioGuide" : "Guide visuel",
+			"access:EntranceOpeningType" : "Type d'ouverture des portes",
+			"access:ManualDoor" : "Ouverture manuelle",
+			"access:PoweredDoor" : "Ouverture via interrupteur",
+			"access:AutomaticDoor" : "Ouverture automatique",
+			"access:FloorAccessType" : "Accès aux étages",
+			"access:Elevator" : "Ascenseur",
+			"access:Stairs" : "Escaliers",
+			"access:EaseofAccess" : "Facilité d'accès",
+			"access:EasyAccess" : "Facile d'accès",
+			"access:DifficultAccess" : "Difficile d'accès"
+		});
+
+		$translateProvider.preferredLanguage("en");
+	}])
+	.run(["GoogleAuth", "$http", "$translate", function(GoogleAuth, $http, $translate) {
 		// if the user is connected, set his token in the HTPP header fields
 		GoogleAuth.isLogin()
 		.then(function(result) {
@@ -21,4 +65,7 @@ angular.module("GalacticHorseChrome",
 		}, function(error) {
 			console.error(error);
 		});
+
+		// set the default language
+		$translate.use("fr");
 	}]);
