69df756921f6536cbc37f8f009789960096e45dd
Giraudux <giraudux@gmail.com>
2015-12-09 13:24:49 +0100

    add authentication (second try)

---
+++ galactichorse/src/main/java/com/galactichorse/Constants.java
@@ -0,0 +1,8 @@
+package com.galactichorse;
+
+public class Constants {
+    public static final String WEB_CLIENT_ID = "528853262624-s4lcimbmpk6j3h6nlbr1rq043iifi81p.apps.googleusercontent.com";
+    public static final String EXTENSIO_CLIENT_ID = "528853262624-cohk31bugnvlss40ctkanqqg51gagcua.apps.googleusercontent.com";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+    public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
